43952310b36c695ff07dfd395c80f84a741e5dbd
Udith <nagarajan.25s@hotmail.com>
2020-11-28 11:21:31 +0000

    Add ZAP tool

---
+++ NFTIntegration/Model/ZapModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NFTIntegration.Model
+{
+    public class ZapModel
+    {
+        [Required]
+        public string Url { get; set; }
+
+        public string Description { get; set; }
+    }
+}
