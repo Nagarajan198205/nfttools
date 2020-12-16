03d33832ac510e5e4bd5795528b57ffe4e7f6d5f
Levin Charles <levincharles-sj@outlook.com>
2020-12-16 20:28:09 +0000

    Added User Roles

--- NFTIntegration/Models/Account/User.cs
+++ NFTIntegration/Models/Account/User.cs
@@ -6,6 +6,7 @@ namespace NFTIntegration.Models.Account
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Username { get; set; }
+        public string Role { get; set; }
         public string Token { get; set; }
         public bool IsDeleting { get; set; }
     }
